b8824d1fc37e7a098e1845ec9b39572f8352489e
m.umair.kn <m.umair.kn@gmail.com>
2026-07-22 22:13:20 +0500

    G: Added LeafNode class and its test cases

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -25,4 +25,21 @@ class HTMLNode:
     def __repr__(self) -> str:
         return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
 
-    
+class LeafNode(HTMLNode):
+    def __init__(
+            self, 
+            tag: str | None, 
+            value: str, 
+            props: dict[str, str] | None = None
+        ) -> None:
+        super().__init__(tag, value, None, props)
+
+    def to_html(self) -> str:
+        if not self.value:
+            raise ValueError("Invalid HTML: should have value")
+        if not self.tag:
+            return self.value
+        return f"<{self.tag}>{self.value}</{self.tag}>"
+
+    def __repr__(self) -> str:
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
