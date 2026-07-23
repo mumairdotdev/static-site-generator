f34047ee4ae06734223c855abaf3f137b8bf98d4
m.umair.kn <m.umair.kn@gmail.com>
2026-07-23 18:54:59 +0500

    K: Added markdown links and images extraction functions

--- src/test_inline.py
+++ src/test_inline.py
@@ -2,6 +2,8 @@ import unittest
 
 from inline import (
     split_nodes_delimiter,
+    extract_markdown_images,
+    extract_markdown_links,
 )
 from textnode import TextNode, TextType
 
@@ -10,7 +12,7 @@ class TestInlineMarkdown(unittest.TestCase):
     def test_delim_bold(self):
         node = TextNode("This is text with a **bolded** word", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "**", TextType.BOLD)
-        self.assertListEqual(
+        self.assertEqual(
             [
                 TextNode("This is text with a ", TextType.TEXT),
                 TextNode("bolded", TextType.BOLD),
@@ -84,6 +86,24 @@ class TestInlineMarkdown(unittest.TestCase):
                 TextNode(" word", TextType.TEXT),
             ],
             new_nodes,
+        )
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a [link](https://boot.dev) and [another link](https://wikipedia.org)"
+        )
+        self.assertListEqual(
+            [
+                ("link", "https://boot.dev"),
+                ("another link", "https://wikipedia.org"),
+            ],
+            matches,
         )
 
 
